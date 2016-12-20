041691178ee9aced20b03350fa92da5102114c7a
saikamaya <ho324rn@gmail.com>
2016-12-21 08:05:36 +0900

    登録APIの大枠を作成

--- app/controllers/register_book_controller.rb
+++ app/controllers/register_book_controller.rb
@@ -3,8 +3,31 @@
 class RegisterBookController < ApplicationController
 
   def register_api
-    res = {'message' => '本棚に2冊登録しました', 'books' => ['book1', 'book2']}
-    render :json => res
-  end
-
+      # The below code is my ruby practice! I delete this later.
+      info = [
+          'isbn', 'title', 'categorCode', 'author', 'editer', 'translator',
+          'publisher', 'publishedDate', 'edition', 'image', 'description']
+      @debug_msg = params
+      # param_chk => isbn, title is must
+      bookId = userCode[:userCode]
+      bookInfo = params[:books]
+      rec = {}
+      for v in info do
+          # set value
+          if v in bookInfo then
+            rec[v] = bookInfo[v]
+          end
+      end
+      # insert record.
+      res = 'Registered 1 book. (title)'
+      @return_msg = res
+      render :json => res
+    # Processes of this api are below.
+    # 100. change return type to json
+    # 200. check parameter(not important)
+    # 300. load existing record
+    # 400. update database
+    # 500. return message
+    # 600. delete erb file(not important)
+    end
 end
