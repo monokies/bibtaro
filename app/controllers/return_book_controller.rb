43df8345b8d14c26c55a270928a7e10ef9791bac
pyohei <mukaishohei76@gmail.com>
2017-01-09 15:58:04 +0900

    返却用のAPIでDB検索と更新ができる様修正。

--- app/controllers/return_book_controller.rb
+++ app/controllers/return_book_controller.rb
@@ -1,8 +1,8 @@
 require "date"
 
 class ReturnBookController < ApplicationController
+  # 返却用API
   def return_api
-    # The below code is my ruby practice! I delete this later.
     book_id = params[:bookId]
     user_code = params[:userCode]
     if params[:lentTo]
@@ -11,6 +11,8 @@ class ReturnBookController < ApplicationController
       lent_to = DateTime.now.strftime("%Y-%m-%d %H:%M:%S")
     end
 
+    # 本当はこの処理の間はトランザクションをかけておくべきだが、
+    # 今は必要最低限の機能に収めておく様にする
     if has_lent_book(book_id, user_code) then
       puts('having!')
       begin
@@ -32,22 +34,28 @@ class ReturnBookController < ApplicationController
     render :json => res, :status => status
   end
 
+  # 貸し出し中の本があるかどうかをチェックする。
   def has_lent_book(book_id, user_code)
-    # Impliment database process after created database.
-    if book_id == '00000000' then
+    # 貸し出し中のステータスは2なので、2で絞る様にする。
+    # 本当はModel側にこのロジックは作るべき？
+    # あと、貸し出しが2件以上ある場合は、NGとすべき？
+    lending_status = 2
+    @lending_book = BookRentalStatus.find_by(bookId: book_id,
+                                            userId: user_code,
+                                            status: lending_status)
+    if @lending_book.nil?
       return false
     end
     return true
   end
 
+  # DBに対して返却処理を実行する
   def return_book(book_id, user_code, lent_to)
-    # Impliment database process after created database.
-    # Currently, this define return `true` for all access!
-    puts('The process of return is unimpliment.')
-    # This is for test.
-    if user_code == '99999999' then
-      raise IOError, '更新時にエラーが発生しました'
-    end
+    lent_status = 3
+    puts('実行!')
+    # インスタンス変数を使うのが正義なのかわからない...
+    @lending_book.status = lent_status
+    @lending_book.save
   end
 
 end
