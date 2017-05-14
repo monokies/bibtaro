3c3498d35fe31d59ab209c55e4f3d79b45bf5160
pyohei <mukaishohei76@gmail.com>
2017-05-14 09:56:25 +0900

    Delete all rails source code.

--- app/helpers/return_book_helper.rb
+++
@@ -1,2 +0,0 @@
-module ReturnBookHelper
-end
--- test/controllers/register_book_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RegisterBookControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
