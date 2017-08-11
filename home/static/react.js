fa61d1a9462ed5141812a4e104f52492eb22146a
pyohei <mukaishohei76@gmail.com>
2017-08-11 22:56:17 +0900

    Add search function.

--- home/static/react.js
+++ home/static/react.js
@@ -1,14 +1,16 @@
-fetch('http://127.0.0.1:8000/api/books').then(
-  function(response) {
-    return response.json();
-}).then(
-  function(json) {
-    var a = json[0].author;
-    const element = (<p>Hello {a}!</p>)
-    
-    ReactDOM.render(
-      element,
-      document.getElementById('app')
-    );
-  }
-);
+function sh() {
+  fetch('http://127.0.0.1:8000/api/books').then(
+    function(response) {
+      return response.json();
+  }).then(
+    function(json) {
+      var a = json[0].author;
+      const element = (<p>Hello {a}!</p>)
+      
+      ReactDOM.render(
+        element,
+        document.getElementById('app')
+      );
+    }
+  );
+};
