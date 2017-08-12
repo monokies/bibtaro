e10c1dfbda718dd8ed401ca4a984ecf212aeac50
pyohei <mukaishohei76@gmail.com>
2017-08-12 22:27:45 +0900

    Refactor and fix to use api result.

--- home/static/react.js
+++ home/static/react.js
@@ -1,34 +1,35 @@
 function book_search() {
-  function test() {
-      var s = "";
-      for (var i=0; i<5; i++) {
-        s += "<p>test</p><br>"
-    }
-      return s
-  };
   fetch('http://127.0.0.1:8000/api/books').then(
     function(response) {
       return response.json();
   }).then(
     function(json) {
-      var a = json[0].author;
+      const header = (
+              <tr>
+                <th></th>
+                <th>著者</th>
+                <th>タイトル</th>
+                <th>発行日</th>
+                <th>画像</th>
+              </tr>
+      )
+      const body = [0].map((n) =>
+              <tr>
+                <td>1</td>
+                <td>{json[n].author}</td>
+                <td>{json[n].title}</td>
+                <td>{json[n].publishedDate}</td>
+                <td>画像</td>
+              </tr>
+      )
       const element = (
-        <table className="table table-bordered">
-          <tr>
-            <th></th>
-            <th>著者</th>
-            <th>タイトル</th>
-            <th>発行日</th>
-            <th>画像</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>{a}</td>
-            <td>タイトル</td>
-            <td>発行日</td>
-            <td>画像</td>
-          </tr>
-          {test()}
+        <table className="table table-sm">
+          <thead>
+            {header}
+          </thead>
+          <tbody>
+            {body}
+          </tbody>
         </table>
       )
       ReactDOM.render(
