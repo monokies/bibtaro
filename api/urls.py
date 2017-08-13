24acac365af38bd31bddfa45955336d6f5ad1c31
pyohei <mukaishohei76@gmail.com>
2017-08-13 21:24:10 +0900

    Add books api

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from .views import UsersViewSet
 
 router = routers.DefaultRouter()
 router.register(r'book_rental_status', BookRentalStatusViewSet)
-router.register(r'books', BooksViewSet)
+router.register(r'books', BooksViewSet, base_name='books')
 router.register(r'users', UsersViewSet)
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 
 # Create your views here.
+#from rest_framework import generics
 from rest_framework import viewsets
 from .models import BookRentalStatus
 from .models import Books
@@ -16,8 +17,28 @@ class BookRentalStatusViewSet(viewsets.ModelViewSet):
 
 
 class BooksViewSet(viewsets.ModelViewSet):
-    queryset = Books.objects.all()
+    #queryset = Books.objects.all()
     serializer_class = BooksSerializer
+
+    def get_queryset(self):
+        queryset = Books.objects.all()
+
+        # TODO:
+        #   - Error handling
+
+        # Title
+        t = self.request.query_params.get('title', None)
+        if t:
+            queryset = queryset.filter(title__contains=t)
+        # Author
+        a = self.request.query_params.get('author', None)
+        if a:
+            queryset = queryset.filter(author__contains=a)
+        # Category
+        c = self.request.query_params.get('category', None)
+        if c:
+            queryset = queryset.filter(category=c)
+        return queryset
 
 
 class UsersViewSet(viewsets.ModelViewSet):
